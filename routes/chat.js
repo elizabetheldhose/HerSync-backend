ddd599679ba22c2a9730e595f0847e307f0fa6a4
elizabeth <elizabetheldhose001@gmail.com>
2026-03-20 06:21:25 +0530

    chat issue fix

--- routes/chat.js
+++ routes/chat.js
@@ -1,16 +1,13 @@
-
 const router = require("express").Router();
 const authMiddleware = require("../middleware/auth");
 const { generateChatResponse } = require("../service/chatService");
 
-
-router.post("/", authMiddleware, async (req, res) => {
+router.post("/chat", authMiddleware, async (req, res) => {
   const { message, history } = req.body;
 
   const response = await generateChatResponse(message, history);
 
   res.json({ reply: response });
 });
-
 
 module.exports = router;
